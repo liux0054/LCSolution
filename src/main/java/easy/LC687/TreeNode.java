77756882be26237b67be0318e26852bdb4381b33
Xue Liu <xuliu@homeaway.com>
2019-03-09 19:01:19 +0800

    Add LC687

---
+++ src/main/java/easy/LC687/TreeNode.java
@@ -0,0 +1,8 @@
+package easy.LC687;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
---
+++ src/test/java/easy/LC687/LC687Test.java
@@ -0,0 +1,36 @@
+package easy.LC687;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LC687Test {
+
+    @Test
+    public void testCase1(){
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(1);
+        root.right.right = new TreeNode(5);
+
+        Assert.assertEquals(2, new LC687().longestUnivaluePath(root));
+    }
+
+    @Test
+    public void testCase2(){
+        TreeNode root = new TreeNode(1);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(4);
+        root.right.right = new TreeNode(5);
+
+        Assert.assertEquals(2, new LC687().longestUnivaluePath(root));
+    }
+
+    @Test
+    public void testCase3(){
+        Assert.assertEquals(0, new LC687().longestUnivaluePath(null));
+    }
+}
