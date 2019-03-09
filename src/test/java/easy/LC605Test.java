e0f854b34432a8b35974c68fbf127f76f0d5f5a5
Xue Liu <xuliu@homeaway.com>
2019-03-09 14:49:08 +0800

    Added LC605

---
+++ src/test/java/easy/LC605Test.java
@@ -0,0 +1,24 @@
+package easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LC605Test {
+
+    @Test
+    public void testCase1(){
+        Assert.assertTrue(new LC605().canPlaceFlowers(new int[]{1,0,0,0,1}, 1));
+    }
+
+    @Test
+    public void testCase2(){
+        Assert.assertFalse(new LC605().canPlaceFlowers(new int[]{1,0,0,0,1}, 2));
+    }
+
+
+    @Test
+    public void testCase3(){
+        Assert.assertTrue(new LC605().canPlaceFlowers(new int[]{1,0,0,0,1,0,0}, 2));
+    }
+}
+
