ffeb54a5809b7c5f30b4a660b51a78110b7eeeee
Xue Liu <xuliu@homeaway.com>
2019-03-10 10:40:29 +0800

    Add LC88

---
+++ src/test/java/easy/LC88Test.java
@@ -0,0 +1,28 @@
+package easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LC88Test {
+
+    @Test
+    public void testCase1(){
+        int[] arr1 = new int[]{1,2,3,0,0,0};
+        new LC88().merge(arr1, 3, new int[]{2, 5, 6}, 3);
+        Assert.assertArrayEquals(new int[]{1,2,2,3,5,6}, arr1);
+    }
+
+    @Test
+    public void testCase2(){
+        int[] arr1 = new int[]{1,2,3,0,0,0};
+        new LC88().merge(arr1, 3, new int[]{}, 0);
+        Assert.assertArrayEquals(new int[]{1,2,3,0,0,0}, arr1);
+    }
+
+    @Test
+    public void testCase3(){
+        int[] arr1 = new int[]{0,0,0,0,0,0};
+        new LC88().merge(arr1, 0, new int[]{1,2,3}, 3);
+        Assert.assertArrayEquals(new int[]{1,2,3,0,0,0}, arr1);
+    }
+}
