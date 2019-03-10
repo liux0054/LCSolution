4130e2d8d10e1e2424abc35cb53312647aeeb002
Xue Liu <xuliu@homeaway.com>
2019-03-10 10:18:51 +0800

    Add LC754

--- src/test/java/easy/LC754Test.java
+++ src/test/java/easy/LC754Test.java
@@ -1,4 +1,27 @@
 package easy;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class LC754Test {
+
+    @Test
+    public void testCase1(){
+        Assert.assertEquals(2, new LC754().reachNumber(3));
+    }
+
+    @Test
+    public void testCase2(){
+        Assert.assertEquals(3, new LC754().reachNumber(2));
+    }
+
+    @Test
+    public void testCase3(){
+        Assert.assertEquals(3, new LC754().reachNumber(-2));
+    }
+
+    @Test
+    public void testCase12(){
+        Assert.assertEquals(7, new LC754().reachNumber(-12));
+    }
 }
