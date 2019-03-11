61650784c8be08dc449794b5230616bc6cfb5b01
Xue Liu <xuliu@homeaway.com>
2019-03-11 17:25:42 +0800

    Add LC26

---
+++ src/main/java/easy/LC26.java
@@ -0,0 +1,28 @@
+package easy;
+
+public class LC26 {
+    public int removeDuplicates(int[] nums) {
+        int begin = 0;
+        int end = 0;
+        int countOfDuplicate = 0;
+        while(end < nums.length - countOfDuplicate){
+            while(end < nums.length - countOfDuplicate && nums[end] == nums[begin]){
+                end++;
+            }
+            if(end - begin > 1){
+                for(int i=begin+1; i<nums.length - countOfDuplicate-1; i++){
+                    if(end + i - begin - 1 <nums.length){
+                        nums[i] = nums[end + i - begin - 1];
+                    }
+                }
+                countOfDuplicate += end - begin -1;
+                begin = begin + 1;
+                end = begin;
+            }else{
+                begin = end;
+                end = begin;
+            }
+        }
+        return nums.length - countOfDuplicate;
+    }
+}
