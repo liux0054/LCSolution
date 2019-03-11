91ca12e6ef7f7b38ccfc732fbcf6d130c71c1f90
Xue Liu <xuliu@homeaway.com>
2019-03-11 15:30:53 +0800

    Add LC342

---
+++ src/main/java/easy/LC342.java
@@ -0,0 +1,9 @@
+package easy;
+
+public class LC342 {
+
+    public boolean isPowerOfFour(int num) {
+        return Integer.bitCount(num) == 1 && (Integer.toBinaryString(num).length()-1)%2==0;
+    }
+
+}
---
+++ src/test/java/easy/LC342Test.java
@@ -0,0 +1,17 @@
+package easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LC342Test {
+
+    @Test
+    public void testCase1(){
+        Assert.assertFalse(new LC342().isPowerOfFour(1));
+    }
+
+    @Test
+    public void testCase2(){
+        Assert.assertFalse(new LC342().isPowerOfFour(2));
+    }
+}
