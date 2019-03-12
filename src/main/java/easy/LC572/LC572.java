4c10ba7386244b507d8e6256e1834eee05ea7022
Xue Liu <xuliu@homeaway.com>
2019-03-12 12:31:11 +0800

    Add LC572

---
+++ src/main/java/easy/LC572/LC572.java
@@ -0,0 +1,26 @@
+package easy.LC572;
+
+public class LC572 {
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+        if(s == null) {
+            return false;
+        }
+        if(s.val == t.val) {
+            if (isSameTree(s, t)) {
+                return true;
+            }
+        }
+        return isSubtree(s.left, t) || isSubtree(s.right, t);
+    }
+
+    private boolean isSameTree(TreeNode s, TreeNode t){
+        if(s == null || t == null){
+            if(s == null && t == null){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        return s.val == t.val && isSameTree(s.left, t.left) && isSameTree(s.right, t.right);
+    }
+}
