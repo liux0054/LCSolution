29a45a3357e8bb0c404584e371cdaf95f2e08c43
Xue Liu <xuliu@homeaway.com>
2019-03-12 12:08:14 +0800

    Add LC198

---
+++ src/main/java/easy/LC198.java
@@ -0,0 +1,19 @@
+package easy;
+
+public class LC198 {
+    public int rob(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        int[][] sums = new int[nums.length][2];
+        sums[0][0] = 0;
+        sums[0][1] = nums[0];
+
+        for(int i=1; i<nums.length; i++){
+            sums[i][0] = Math.max(sums[i-1][0], sums[i-1][1]);
+            sums[i][1] = sums[i-1][0] + nums[i];
+        }
+
+        return Math.max(sums[nums.length-1][0], sums[nums.length-1][1]);
+    }
+}
