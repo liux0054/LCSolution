f8e5c2e4aa80cb5dd9e13c8826d2acfee142b6e6
Xue Liu <xuliu@homeaway.com>
2019-03-12 11:28:43 +0800

    Add LC970

--- src/test/java/easy/LC970Test.java
+++ src/test/java/easy/LC970Test.java
@@ -12,14 +12,16 @@ public class LC970Test {
     @Test
     public void testCase1(){
 
-        List actual = new LC970().powerfulIntegers(2, 3, 10);
+        List<Integer> actual = new LC970().powerfulIntegers(2, 3, 10);
         Collections.sort(actual);
-        List expected = Arrays.asList(2,3,4,5,7,9,10);
+        List<Integer> expected = Arrays.asList(2,3,4,5,7,9,10);
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void testCase2(){
-        Assert.assertEquals(6, new LC970().powerfulIntegers(3, 5, 15).size());
+        List<Integer> actual = new LC970().powerfulIntegers(3, 5, 15);
+        List<Integer> expected = Arrays.asList(2,4,6,8,10,14);
+        Assert.assertTrue(actual.containsAll(expected) && expected.containsAll(actual));
     }
 }
