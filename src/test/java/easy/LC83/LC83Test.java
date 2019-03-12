3ccd325a34269005cdb3d48d169e1e64dabca402
Xue Liu <xuliu@homeaway.com>
2019-03-12 11:55:48 +0800

    Add LC83

---
+++ src/test/java/easy/LC83/LC83Test.java
@@ -0,0 +1,48 @@
+package easy.LC83;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LC83Test {
+
+    @Test
+    public void testCase1(){
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
+        ListNode actual = new LC83().deleteDuplicates(head);
+
+        ListNode expected = new ListNode(1);
+        expected.next = new ListNode(2);
+        while(actual != null || expected != null){
+            Assert.assertEquals(actual.val, expected.val);
+            actual = actual.next;
+            expected = expected.next;
+        }
+        Assert.assertNull(actual);
+        Assert.assertNull(expected);
+    }
+
+    @Test
+    public void testCase2(){
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(1);
+        head.next.next = new ListNode(2);
+        head.next.next.next= new ListNode(3);
+        head.next.next.next.next = new ListNode(3);
+        ListNode actual = new LC83().deleteDuplicates(head);
+
+        ListNode expected = new ListNode(1);
+        expected.next = new ListNode(2);
+        expected.next.next = new ListNode(3);
+        while(actual != null || expected != null){
+            Assert.assertEquals(actual.val, expected.val);
+            actual = actual.next;
+            expected = expected.next;
+        }
+        Assert.assertNull(actual);
+        Assert.assertNull(expected);
+    }
+}
