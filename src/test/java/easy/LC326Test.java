c7c01453f9af9131c4466bc181b4a7e3a491669c
Xue Liu <xuliu@homeaway.com>
2019-03-13 13:34:16 +0800

    Add LC326

---
+++ src/test/java/easy/LC326Test.java
@@ -0,0 +1,32 @@
+package easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LC326Test {
+
+    @Test
+    public void testCase1(){
+        Assert.assertTrue(new LC326().isPowerOfThree(1));
+    }
+
+    @Test
+    public void testCase2(){
+        Assert.assertTrue(new LC326().isPowerOfThree(27));
+    }
+
+    @Test
+    public void testCase3(){
+        Assert.assertTrue(new LC326().isPowerOfThree(9));
+    }
+
+    @Test
+    public void testCase4(){
+        Assert.assertFalse(new LC326().isPowerOfThree(0));
+    }
+
+    @Test
+    public void testCase5(){
+        Assert.assertFalse(new LC326().isPowerOfThree(45));
+    }
+}
