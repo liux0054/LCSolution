05fe3412f896eee4df8e406834edca738fc18b09
Xue Liu <xuliu@homeaway.com>
2019-03-16 22:55:29 +0800

    Add LC414, 581, 665

---
+++ src/main/java/easy/LC665.java
@@ -0,0 +1,28 @@
+package easy;
+
+public class LC665 {
+    public boolean checkPossibility(int[] nums) {
+       for(int i=0; i<nums.length-1; i++){
+          if(nums[i] > nums[i+1]){
+              if(i+2 < nums.length && nums[i] > nums[i+2]){
+                  nums[i] = nums[i+1];
+              }else{
+                  nums[i+1] = nums[i];
+              }
+              break;
+          }
+       }
+
+       int[] diff = new int[nums.length - 1];
+       for(int i=0; i<nums.length-1; i++){
+           diff[i] = nums[i+1] - nums[i];
+       }
+
+       for(int dif:diff){
+           if(dif < 0){
+               return false;
+           }
+       }
+       return true;
+    }
+}
