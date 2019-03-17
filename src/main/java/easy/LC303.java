4da07f4d9b37ab82772af24f311a7bee16253c9b
Xue Liu <xuliu@homeaway.com>
2019-03-17 09:58:00 +0800

    Add LC303

--- src/main/java/easy/LC303.java
+++ src/main/java/easy/LC303.java
@@ -11,14 +11,18 @@ class NumArray{
     public NumArray(int[] nums) {
         this.nums = nums;
         sums = new int[nums.length];
-        sums[0] = nums[0];
-        for(int i=1; i<nums.length; i++){
-            sums[i] = sums[i-1] + nums[i];
+        if(this.nums != null && this.nums.length > 0){
+            sums[0] = nums[0];
+            for(int i=1; i<nums.length; i++){
+                sums[i] = sums[i-1] + nums[i];
+            }
         }
     }
 
     public int sumRange(int i, int j) {
-        if(i >= sums.length || j >= sums.length) return 0;
+        if(i >= sums.length || j >= sums.length) {
+            return 0;
+        }
         return i>=1 ? (sums[j]-sums[i-1]) : sums[j];
     }
 }
---
+++ src/main/java/easy/LC475.java
@@ -0,0 +1,7 @@
+//package easy;
+//
+//public class LC475 {
+//    public int findRadius(int[] houses, int[] heaters) {
+//
+//    }
+//}
