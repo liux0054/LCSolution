7759dab74eb428c5e611ee43b9107b118b8bad14
Xue Liu <xuliu@homeaway.com>
2019-03-18 00:23:07 +0800

    Add LC703, LC704, LC836, LC925

---
+++ src/test/java/easy/LC836Test.java
@@ -0,0 +1,23 @@
+package easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LC836Test {
+    @Test
+    public void testCase1(){
+        Assert.assertTrue(new LC836().isRectangleOverlap(new int[]{0,0,2,2}, new int[]{1,1,3,3}));
+    }
+
+
+    @Test
+    public void testCase2(){
+        Assert.assertFalse(new LC836().isRectangleOverlap(new int[]{0,0,1,1}, new int[]{1,0,2,1}));
+    }
+
+
+    @Test
+    public void testCase3(){
+        Assert.assertTrue(new LC836().isRectangleOverlap(new int[]{4,4,14,7}, new int[]{4,3,8,8}));
+    }
+}
