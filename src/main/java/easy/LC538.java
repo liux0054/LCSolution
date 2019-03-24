46b219217c21ee478a73726d2f2b0b430684ed1c
Xue Liu <xuliu@homeaway.com>
2019-03-24 15:24:59 +0800

    add LC538, LC328

--- src/main/java/easy/LC538.java
+++ src/main/java/easy/LC538.java
@@ -4,6 +4,19 @@ import common.TreeNode;
 
 public class LC538 {
     public TreeNode convertBST(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+        if(root.right != null){
+            convertBST(root.right);
+            root.val += root.right.val;
+        }
 
+        if(root.left != null){
+            root.left.val += root.val;
+            convertBST(root.left);
+        }
+
+        return root;
     }
 }
---
+++ src/main/java/medium/LC328.java
@@ -0,0 +1,43 @@
+package medium;
+
+import common.ListNode;
+
+public class LC328 {
+    public ListNode oddEvenList(ListNode head) {
+        ListNode third = head;
+        if(third == null){
+            return null;
+        }
+
+        ListNode second = head.next;
+        if(second == null){
+            return head;
+        }
+
+        ListNode first = head.next.next;
+        if(first == null){
+            return head;
+        }
+
+        ListNode odd = third;
+        ListNode even = second;
+        odd.next = first;
+
+        while (first != null && second != null){
+            second.next = first.next;
+            second = second.next;
+            if(second != null){
+                first.next = second.next;
+                first = first.next;
+            }
+        }
+
+        ListNode pOdd = odd;
+        while(pOdd.next != null){
+            pOdd = pOdd.next;
+        }
+
+        pOdd.next = even;
+        return odd;
+    }
+}
