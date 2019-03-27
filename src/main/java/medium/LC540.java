43398f0e4579764176fd697c1455ab64a818ab0b
Xue Liu <xuliu@homeaway.com>
2019-03-27 09:41:42 +0800

    Add LC540, LC921, LC937

---
+++ src/main/java/medium/LC540.java
@@ -0,0 +1,18 @@
+package medium;
+
+public class LC540 {
+    public int singleNonDuplicate(int[] nums) {
+       if(nums.length == 1){
+           return nums[0];
+       }
+       int i=0;
+
+       while(i<nums.length-1){
+           if(nums[i] != nums[i+1]){
+               return nums[i];
+           }
+           i += 2;
+       }
+       return nums[i];
+    }
+}
---
+++ src/main/java/medium/LC921.java
@@ -0,0 +1,16 @@
+//package medium;
+//
+//import java.util.Stack;
+//
+//public class LC921 {
+//    public int minAddToMakeValid(String S) {
+//        Stack<Character> stack = new Stack<>();
+//        for(Character c:S.toCharArray()){
+//            if(c == '('){
+//                stack.push(c);
+//            }else{
+//
+//            }
+//        }
+//    }
+//}
