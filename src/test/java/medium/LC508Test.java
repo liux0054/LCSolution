0dd6eeb237eb1cc7fa97f488a4420a9c4ac990d2
Xue Liu <xuliu@homeaway.com>
2019-03-27 12:20:15 +0800

    Add LC508

---
+++ src/test/java/medium/LC508Test.java
@@ -0,0 +1,30 @@
+package medium;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import common.TreeNode;
+
+public class LC508Test {
+    @Test
+    public void testCase1(){
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(-3);
+        Assert.assertArrayEquals(new int[]{2, -3, 4}, new LC508().findFrequentTreeSum(root));
+    }
+
+    @Test
+    public void testCase2(){
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(2);
+        root.right = new TreeNode(-5);
+        Assert.assertArrayEquals(new int[]{2}, new LC508().findFrequentTreeSum(root));
+    }
+
+    @Test
+    public void testCase3(){
+        TreeNode root = new TreeNode(5);
+        Assert.assertArrayEquals(new int[]{5}, new LC508().findFrequentTreeSum(root));
+    }
+}
