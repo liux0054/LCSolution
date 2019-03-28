ef8cb7f70c8f75234fcc372b8eb2d50956d0282e
Xue Liu <xuliu@homeaway.com>
2019-03-28 14:26:28 +0800

    Add LC238, LC347, LC451

---
+++ src/test/java/medium/LC238Test.java
@@ -0,0 +1,16 @@
+package medium;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LC238Test {
+    @Test
+    public void testCase1(){
+        Assert.assertArrayEquals(new int[]{24,12,8,6}, new LC238().productExceptSelf(new int[]{1,2,3,4}));
+    }
+
+    @Test
+    public void testCase2(){
+        Assert.assertArrayEquals(new int[]{1,2}, new LC238().productExceptSelf(new int[]{2,1}));
+    }
+}
