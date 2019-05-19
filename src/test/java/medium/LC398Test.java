f1c4d98ef0f81b586bdaf87dabafd9ba14f0963d
Xue Liu <xuliu@homeaway.com>
2019-05-19 16:40:40 +0800

    LC462

---
+++ src/test/java/medium/LC398Test.java
@@ -0,0 +1,17 @@
+package medium;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LC398Test {
+
+    @Test
+    public void testCase1(){
+        LC398 lc398 = new LC398();
+        LC398.Solution sl = lc398.new Solution(new int[] {1,2,3,3,3});
+        Assert.assertEquals(0, sl.pick(1));
+        Assert.assertEquals(1, sl.pick(2));
+        //Assert.assertEquals(2, sl.pick(3));
+    }
+}
---
+++ src/test/java/medium/LC462Test.java
@@ -0,0 +1,17 @@
+package medium;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LC462Test {
+    @Test
+    public void testCase1(){
+        Assert.assertEquals(2, new LC462().minMoves2(new int[]{1,2,3}));
+    }
+
+    @Test
+    public void testCase2(){
+        Assert.assertEquals(0, new LC462().minMoves2(new int[]{2}));
+    }
+
+}
