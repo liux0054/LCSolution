47b9cff9fb52363786a83ba6483cc8245815751a
Xue Liu <xuliu@homeaway.com>
2019-06-02 22:33:49 +0800

    Add LC171

---
+++ src/test/java/easy/lc1tolc200/LC171Test.java
@@ -0,0 +1,29 @@
+package easy.lc1tolc200;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LC171Test {
+
+    @Test
+    public void testCase1(){
+        LC171 lc171 = new LC171();
+        LC171.Solution solution = lc171.new Solution();
+        Assert.assertEquals(1, solution.titleToNumber("A"));
+    }
+
+
+    @Test
+    public void testCase2(){
+        LC171 lc171 = new LC171();
+        LC171.Solution solution = lc171.new Solution();
+        Assert.assertEquals(28, solution.titleToNumber("AB"));
+    }
+
+    @Test
+    public void testCase3(){
+        LC171 lc171 = new LC171();
+        LC171.Solution solution = lc171.new Solution();
+        Assert.assertEquals(701, solution.titleToNumber("ZY"));
+    }
+}
