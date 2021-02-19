21377e9e103b40d371b2730e269e4c4e568b7edc
xgong <xgong@flextrade.com>
2021-02-19 21:28:59 +0800

    LC79

--- src/main/java/medium/LC79.java
+++ src/main/java/medium/LC79.java
@@ -5,7 +5,7 @@ public class LC79 {
         boolean[][] used = new boolean[200][200];
         for (int i=0; i<board.length; i++) {
             for (int j=0; j<board[i].length; j++) {
-                if (board[i][j] == word.charAt(0) && match(board, used,0, i, j, word)) {
+                if (match(board, used,0, i, j, word)) {
                     return true;
                 }
             }
@@ -19,19 +19,19 @@ public class LC79 {
                 return true;
             }
             used[row][column] = true;
-            boolean up = row > 0 && (!used[row-1][column]) && board[row-1][column] == word.charAt(index+1) && match(board, used, index+1, row-1, column, word);
+            boolean up = row > 0 && !used[row-1][column] && match(board, used, index+1, row-1, column, word);
             if (up) {
                 return true;
             }
-            boolean down = row < board.length - 1 && (!used[row+1][column]) && board[row+1][column] == word.charAt(index+1) && match(board, used, index+1, row+1, column, word);
+            boolean down = row < board.length - 1 && !used[row+1][column] && match(board, used, index+1, row+1, column, word);
             if (down) {
                 return true;
             }
-            boolean left = column > 0 && (!used[row][column-1]) && board[row][column-1] == word.charAt(index+1) && match(board, used, index+1, row, column-1, word);
+            boolean left = column > 0 && !used[row][column-1] && match(board, used, index+1, row, column-1, word);
             if (left) {
                 return true;
             }
-            boolean right = column < board[row].length - 1 && board[row][column+1] == word.charAt(index+1) && !used[row][column+1] && match(board, used, index+1, row, column+1, word);
+            boolean right = column < board[row].length - 1 && !used[row][column+1] && match(board, used, index+1, row, column+1, word);
             if (right) {
                 return true;
             }
